a2d53a364cc7e748e69d1008e8999fd1eadb53fc
indieterminacy <indieterminacy@nortonspeal.com>
2016-10-23 23:12:03 +0200

    Git repo covering loading requirements

---
+++ oqo_loading.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/zsh_reload
+####################
+# oq SystemMD
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/systemd
+####################
+# oq Wakeonlan
+####################
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/wakeonlan
